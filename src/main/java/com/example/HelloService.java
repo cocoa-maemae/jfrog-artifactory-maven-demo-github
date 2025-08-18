5a3662c8ec5cb63341aad4b2c56b4c3a39189140
Tomoki Maeda <tomoki.maeda@tomoki.maeda-mac>
2025-08-18 22:21:27 +0900

    add a sample maven package

---
+++ src/main/java/com/example/HelloService.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class HelloService {
+    public String greet(String name) {
+        if (name == null || name.isBlank()) {
+            return "Hello";
+        }
+        return "Hello, " + name + "!";
+    }
+}
+
+
